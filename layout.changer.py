7a42a6ec0b702104864f0e8a17df9560fcb9e312
akstatis <solmatvei8@gmail.com>
2024-07-05 15:28:44 +1000

    Add files via upload

---
+++ layout.changer.py
@@ -0,0 +1,75 @@
+text = input()
+
+
+def layout_changer():
+    for norm_text in text:
+        if norm_text == "q":
+            print('й', end='')
+        if norm_text == "w":
+            print('ц', end='')
+        if norm_text == "e":
+            print('у', end='')
+        if norm_text == "r":
+            print('к', end='')
+        if norm_text == "t":
+            print('е', end='')
+        if norm_text == "y":
+            print('н', end='')
+        if norm_text == "u":
+            print('г', end='')
+        if norm_text == "i":
+            print('ш', end='')
+        if norm_text == "o":
+            print('щ', end='')
+        if norm_text == "p":
+            print('з', end='')
+        if norm_text == "[":
+            print('х', end='')
+        if norm_text == "]":
+            print('ъ', end='')
+        if norm_text == "a":
+            print('ф', end='')
+        if norm_text == "s":
+            print('ы', end='')
+        if norm_text == "d":
+            print('в', end='')
+        if norm_text == "f":
+            print('а', end='')
+        if norm_text == "g":
+            print('п', end='')
+        if norm_text == "h":
+            print('р', end='')
+        if norm_text == "j":
+            print('о', end='')
+        if norm_text == "k":
+            print('л', end='')
+        if norm_text == "l":
+            print('д', end='')
+        if norm_text == ";":
+            print('ж', end='')
+        if norm_text == "'":
+            print('э', end='')
+        if norm_text == "z":
+            print('я', end='')
+        if norm_text == "x":
+            print('ч', end='')
+        if norm_text == "c":
+            print('с', end='')
+        if norm_text == "v":
+            print('м', end='')
+        if norm_text == "b":
+            print('и', end='')
+        if norm_text == "n":
+            print('т', end='')
+        if norm_text == "m":
+            print('ь', end='')
+        if norm_text == ",":
+            print('б', end='')
+        if norm_text == ".":
+            print('ю', end='')
+        if norm_text == "/":
+            print('.', end='')
+        if norm_text == "`":
+            print('ёё', end='')
+
+layout_changer()
